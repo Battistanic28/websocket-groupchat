2ccfde1934b1126bd4611bae759a5b0469daeab9
Nick Battista <nickbattista@Nick.Battista>
2021-05-22 11:48:25 -0600

    added /joke easter egg

---
+++ ChatUser.js
@@ -0,0 +1,90 @@
+/** Functionality related to chatting. */
+
+// Room is an abstraction of a chat channel
+const Room = require('./Room');
+const axios = require('axios');
+
+/** ChatUser is a individual connection from client -> server to chat. */
+
+class ChatUser {
+  /** make chat: store connection-device, rooom */
+
+  constructor(send, roomName) {
+    this._send = send; // "send" function for this user
+    this.room = Room.get(roomName); // room user will be in
+    this.name = null; // becomes the username of the visitor
+
+    console.log(`created chat in ${this.room.name}`);
+  }
+
+  /** send msgs to this client using underlying connection-send-function */
+
+  send(data) {
+    try {
+      this._send(data);
+    } catch {
+      // If trying to send to a user fails, ignore it
+    }
+  }
+
+  /** handle joining: add to room members, announce join */
+
+  handleJoin(name) {
+    this.name = name;
+    this.room.join(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} joined "${this.room.name}".`
+    });
+  }
+
+  /** handle a chat: broadcast to room. */
+
+  handleChat(text) {
+    this.room.broadcast({
+      name: this.name,
+      type: 'chat',
+      text: text
+    });
+  }
+
+  handlePrivateChat(text) {
+    this.room.private({
+      name: "server",
+      type: 'chat',
+      text: text
+    }, user)
+  }
+
+  async tellJoke() {
+    let res = await axios.get('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single')
+    let joke = (res.data.joke);
+    this.handleChat(joke);
+  }
+  /** Handle messages from client:
+   *
+   * - {type: "join", name: username} : join
+   * - {type: "chat", text: msg }     : chat
+   */
+
+  handleMessage(jsonData) {
+    let msg = JSON.parse(jsonData);
+    console.log(msg)
+    if (msg.type === 'join') this.handleJoin(msg.name);
+    else if (msg.text === '/joke') this.handleChat(msg.text), this.tellJoke();
+    else if (msg.type === 'chat') this.handleChat(msg.text);
+    else throw new Error(`bad message: ${msg.type}`);
+  }
+
+  /** Connection was closed: leave room, announce exit to others */
+
+  handleClose() {
+    this.room.leave(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} left ${this.room.name}.`
+    });
+  }
+}
+
+module.exports = ChatUser;
